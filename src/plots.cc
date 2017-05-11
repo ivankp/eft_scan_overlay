5ee3ca3195581f9a1ff1f9bc35683617ce8eb82a
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2017-05-11 23:27:25 +0200

    plots: reading bin values

--- src/plots.cc
+++ src/plots.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
+// #include <unordered_set>
 
 #include <boost/filesystem.hpp>
 
@@ -21,53 +22,13 @@ struct directory_range {
 
 struct hist {
   std::string title;
+  struct bin {
+    double xlow, xhigh;
+    std::vector<double> vals;
+  };
+  std::vector<bin> bins;
+  bool filled = false;
 };
-
-struct wc_point {
-  static std::unordered_set<std::string> hists_names;
-  std::unordered_map<std::string,double> wc; // Wilson coeff's values
-  std::unordered_map<std::string,hist> hists;
-  wc_point(const std::string& wc_file, const std::string& yoda_file) {
-    { // read Wilson coeffs
-      std::string name;
-      double value;
-      std::ifstream f(wc_file);
-      while (f >> name >> value) { wc.emplace(std::move(name),value); }
-    }
-    { // read yoda file
-      std::string line, name;
-      auto front = [&line](size_t n){ return line.substr(0,n); };
-      auto back  = [&line](size_t n){ return line.substr(n); };
-      std::ifstream f(yoda_file);
-      bool inside = false;
-      hist *h = nullptr;
-      while (std::getline(f,line)) {
-        if (!inside) {
-          if (front(18)!="BEGIN YODA_HISTO1D") continue;
-          name = line.substr(line.rfind('/')+1);
-          if (!hists_names.count(name)) continue;
-          inside = true;
-          h = &hists.emplace(std::piecewise_construct,
-              std::forward_as_tuple(std::move(name)),
-              std::tie()
-            ).first->second;
-        } else {
-          if (front(16)=="END YODA_HISTO1D") {
-            inside = false;
-            continue;
-          } else if (front(6)=="Title=") {
-            h->title = back(6);
-          }
-        }
-      } // end while
-    }
-  } // end constructor
-};
-std::unordered_set<std::string> wc_point::hists_names {
-  "pT_yy"
-};
-
-// template <class... T> struct type_test;
 
 int main(int argc, char* argv[]) {
   if (argc!=2) {
@@ -76,7 +37,15 @@ int main(int argc, char* argv[]) {
   }
   std::string wc_file("param.dat"), yoda_file("Higgs.yoda");
 
-  std::vector<wc_point> points;
+  std::vector<std::unordered_map<std::string,double>> wc; // Wilson coeff's values
+  std::unordered_map<std::string,hist> hists { // values from yoda histograms
+    {"nJet30",{}},
+    {"pT_yy",{}},
+    {"pT_j1",{}},
+    {"m_jj",{}},
+    {"absdphi_jj",{}},
+    {"dphi_jj",{}}
+  };
   
   for (const auto& dir : directory_range(argv[1])) {
     cout <<"\033[34;1m"<< dir.path().filename().native() <<"\033[0m"<< endl;
@@ -87,20 +56,98 @@ int main(int argc, char* argv[]) {
       else if (name==yoda_file) { yoda_found = true; }
       if (wc_found && yoda_found) break;
     }
-    if (!wc_found)
-      cerr << "\033[31mNo param.dat in " << dir << "\033[0m" << endl;
-    if (!yoda_found)
-      cerr << "\033[31mNo Higgs.yoda in " << dir << "\033[0m" << endl;
-    if (!(wc_found || yoda_found)) continue;
-
-    std::string path = dir.path().string()+'/';
-    points.emplace_back(path+wc_file,path+yoda_file);
-
-    for (const auto& h : points.back().hists) { // TEST
-      cout << "  " << h.first << ' '
-           << h.second.title << endl;
+    if (!wc_found) {
+      cerr << "\033[31mNo " <<   wc_file << " in " << dir << "\033[0m" << endl;
+      continue;
     }
-  }
+    if (!yoda_found) {
+      cerr << "\033[31mNo " << yoda_file << " in " << dir << "\033[0m" << endl;
+      continue;
+    }
+
+    const std::string path = dir.path().string()+'/';
+    // ==============================================================
+    { // read Wilson coeffs
+      wc.emplace_back();
+      std::string name;
+      double value;
+      std::ifstream f(path+wc_file);
+      while (f >> name >> value) { wc.back().emplace(std::move(name),value); }
+    }
+    { // read yoda file
+      std::string line, name;
+      auto front = [&line](size_t n){ return line.substr(0,n); };
+      auto back  = [&line](size_t n){ return line.substr(n); };
+      bool state_hist = false, state_bins = false;
+      size_t bin_i = 0;
+      hist *h = nullptr;
+      std::ifstream f(path+yoda_file);
+      while (std::getline(f,line)) {
+        if (!state_hist) {
+          if (front(18)!="BEGIN YODA_HISTO1D") continue;
+          name = line.substr(line.rfind('/')+1);
+          const auto it = hists.find(name);
+          if (it==hists.end()) continue;
+          state_hist = true;
+          h = &it->second;
+        } else {
+          if (front(16)=="END YODA_HISTO1D") {
+            state_hist = false;
+            state_bins = false;
+            bin_i = 0;
+            if (!h->filled) h->filled = true;
+            continue;
+          } else if (state_bins) { // read histogram bin
+            // ------------------------------------------------------
+
+            double xlow, xhigh, sumw;
+            std::stringstream(line) >> xlow >> xhigh >> sumw;
+
+            hist::bin *b;
+            if (h->filled) {
+              try {
+                b = &h->bins.at(bin_i);
+              } catch(...) {
+                cerr << "\033[31mnbins mismatch in " << dir
+                     << " : " << name << "\033[0m" << endl;
+                return 1;
+              }
+              if (xlow!=b->xlow) {
+                cerr << "\033[31mxlow mismatch in " << dir
+                     << " : " << name << "\033[0m" << endl;
+                return 1;
+              }
+              if (xhigh!=b->xhigh) {
+                cerr << "\033[31mxhigh mismatch in " << dir
+                     << " : " << name << "\033[0m" << endl;
+                return 1;
+              }
+            } else {
+              h->bins.emplace_back();
+              b = &h->bins.back();
+              b->xlow = xlow;
+              b->xhigh = xhigh;
+            }
+            b->vals.push_back(sumw);
+            ++bin_i;
+
+            // ------------------------------------------------------
+          } else if (front(6)=="Title=") {
+            h->title = back(6);
+          } else if (front(6)=="# xlow") {
+            state_bins = true;
+          }
+        }
+      } // end while
+    }
+    // ==============================================================
+
+    for (const auto& h : hists) { // TEST
+      cout << "  " << h.first
+           << ' ' << h.second.title
+           << ' ' << h.second.bins.size() << endl;
+    }
+  } // end dir loop
 
   return 0;
 }
