9356124e85df50144d1ce9354aea6e1dc792c042
Ivan Pogrebnyak <ivan.pogrebnyak@gmail.com>
2017-05-12 00:30:49 +0200

    plots: yrange

--- src/plots.cc
+++ src/plots.cc
@@ -9,6 +9,7 @@
 
 #include <TCanvas.h>
 #include <TH1.h>
+#include <TAxis.h>
 
 using std::cout;
 using std::cerr;
@@ -27,8 +28,10 @@ struct hist {
   struct bin {
     double xlow, xhigh;
     std::vector<double> vals;
+    inline double operator[](size_t i) const noexcept { return vals[i]; }
   };
   std::vector<bin> bins;
+  inline const auto& operator[](size_t i) const noexcept { return bins[i]; }
   unsigned ncwvals = 0;
 };
 
@@ -91,7 +94,7 @@ int main(int argc, char* argv[]) {
           const auto it = hists.find(name);
           if (it==hists.end()) continue;
           state_hist = true;
-          h = &it->second;
+          h = &it->second; // hist pointer
         } else {
           if (front(16)=="END YODA_HISTO1D") {
             state_hist = false;
@@ -164,6 +167,44 @@ int main(int argc, char* argv[]) {
     first = false;
   }
   cout << "\nNum coeff variations: " << ncwvals <<'\n'<< endl;
+
+  // Draw ===========================================================
+
+  TCanvas canv;
+
+  canv.SaveAs("plot.pdf[");
+  for (const auto& yh : hists) {
+    const unsigned nbins = yh.second.bins.size();
+    
+    double ymin = 1e5, ymax = -1e5;
+    for (unsigned j=0; j<ncwvals; ++j) {
+      for (unsigned i=0; i<nbins; ++i) {
+        const double y = yh.second[i][j];
+        if (y < ymin) ymin = y;
+        if (y > ymax) ymax = y;
+      }
+    }
+    std::tie(ymin,ymax) = std::forward_as_tuple(
+      1.05556*ymin - 0.05556*ymax,
+      1.05556*ymax - 0.05556*ymin);
+
+    TH1D h(yh.first.c_str(),yh.second.title.c_str(),nbins,0,1);
+    h.SetStats(false);
+    h.SetLineWidth(2);
+    // h.SetLineColor(46);
+    h.GetYaxis()->SetRangeUser(ymin,ymax);
+
+    for (unsigned j=0; j<ncwvals; ++j) {
+      for (unsigned i=0; i<nbins; ++i)
+        h.SetBinContent(i+1,yh.second[i][j]);
+      if (j) h.DrawCopy("same");
+      else {
+        h.DrawCopy();
+      }
+    }
+    canv.SaveAs("plot.pdf");
+  }
+  canv.SaveAs("plot.pdf]");
 
   return 0;
 }
